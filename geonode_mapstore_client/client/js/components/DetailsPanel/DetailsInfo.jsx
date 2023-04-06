bfabaca8055a152686a6744096635fd353ef6047
Henning Bredel <h.bredel@52north.org>
2023-04-06 16:49:50 +0200

    Renders attribute table just once

--- geonode_mapstore_client/client/js/components/DetailsPanel/DetailsInfo.jsx
+++ geonode_mapstore_client/client/js/components/DetailsPanel/DetailsInfo.jsx
@@ -208,7 +208,7 @@ function DetailsInfo({
                             }
                         }
                         getAttributes();
-                    })
+                    }, [])
                 }
                 return (
                     <Tab key={idx} eventKey={tab?.id} title={<DetailInfoFieldLabel field={tab} />}>
