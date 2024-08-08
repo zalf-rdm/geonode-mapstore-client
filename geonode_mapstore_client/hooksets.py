078fdebc7eb6a548a7becaa7790e23c98bbe5536
Henning Bredel <h.bredel@52north.org>
2024-08-08 15:33:46 +0200

    Redirect from editor to tabular-collection
    Resolves: https://github.com/GeoNodeUserGroup-DE/geonode-dev-datapackage/issues/28

--- geonode_mapstore_client/hooksets.py
+++ geonode_mapstore_client/hooksets.py
@@ -114,6 +114,10 @@ class MapStoreHookSet(BaseHookSet):
         return resource_list_url("map")
 
     def map_detail_url(self, resource):
+        map_resource_type = self.get_map_resource_type(resource)
+        return resource_detail_url(map_resource_type, resource.id)
+
+    def get_map_resource_type(self, resource):
         from geonode.maps.models import Map
         map = Map.objects.get(id=resource.id)
         # alternative route for tabular data collection
@@ -122,8 +126,8 @@ class MapStoreHookSet(BaseHookSet):
             tabular_data_collection = layer.subtype == "tabular"
             if not tabular_data_collection:
                 break
-        
-        return resource_detail_url("map", resource.id) if not tabular_data_collection else resource_detail_url("tabular-collection", resource.id)
+        return "map" if not tabular_data_collection else "tabular-collection"
+
 
     # def map_download_template(self, context=None):
     #    return 'geonode-mapstore-client/legacy/map_view.html'
@@ -203,6 +207,8 @@ class MapStoreHookSet(BaseHookSet):
         if resource.subtype == '3dtiles':
             return resource_detail_url(resource.subtype, resource_identifier)
         resource_type = "tabular" if resource.subtype == "tabular" else resource.resource_type
+        if resource.resource_type == "map":
+            resource_type = self.get_map_resource_type(resource)
         return resource_detail_url(resource_type, resource_identifier)
 
     def get_absolute_url(self, instance):
