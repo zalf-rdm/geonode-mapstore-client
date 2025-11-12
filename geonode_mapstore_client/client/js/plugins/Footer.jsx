c1b118d1dc446355880e36fee9fe4d7163b558c3
silvadealmeida <igotode@gmail.com>
2025-11-12 12:02:14 +0000

    .

--- geonode_mapstore_client/client/js/plugins/Footer.jsx
+++ geonode_mapstore_client/client/js/plugins/Footer.jsx
@@ -48,10 +48,10 @@ const Footer = () => (
                             <li className="mb-2">
                                 <a href="#" className="link-light">How to Cite Us</a>
                             </li>
-                            <li>
+                            <li className="mb-2">
                                 <a href="#" className="link-light">Imprint</a>
                             </li>
-                            <li>
+                            <li className="mb-2">
                                 <a href="#" className="link-light ">Privacy</a>
                             </li>
                         </ul>
@@ -63,17 +63,14 @@ const Footer = () => (
 
                         {/* Icons: centered on mobile, left-aligned on md+ */}
                         <div className="d-flex justify-content-center justify-content-md-start gap-3 mb-4">
-                            <a href="#" className="fs-5 text-light" title="Twitter" aria-label="Twitter"><i className="bi bi-twitter"></i></a>
-                            <a href="#" className="fs-5 text-light" title="GitHub" aria-label="GitHub"><i className="bi bi-github"></i></a>
-                            <a href="#" className="fs-5 text-light" title="LinkedIn" aria-label="LinkedIn"><i className="bi bi-linkedin"></i></a>
-                            <a href="#" className="fs-5 text-light" title="Email" aria-label="Email"><i className="bi bi-envelope"></i></a>
+                            <h4 className="fw-semibold mb-2 fs-7 text-light">Certified by</h4>
+                            <img src="/static/img/logo_core_trust_seal_white.png" alt="Core Trust Seal" className="img-fluid mx-auto mx-md-0" style={{ maxHeight: '50px' }} />
                         </div>
 
                         {/* Logo: centered on mobile, left-aligned on md+ */}
                         <div className="text-start text-md-start">
                             <h4 className="fw-semibold mb-2 fs-7 text-light">Powered by</h4>
-                            <img src="/static/img/logo_zalf_square.jpg" alt="ZALF" className="img-fluid mx-auto mx-md-0" style={{ maxHeight: '50px' }} />
-                            <img src="/static/img/logo_core_trust_seal.png" alt="Core Trust Seal" className="img-fluid mx-auto mx-md-0" style={{ maxHeight: '50px' }} />
+                            <img src="/static/img/logo_zalf_white.jpg" alt="ZALF" className="img-fluid mx-auto mx-md-0" style={{ maxHeight: '50px' }} />
 
                         </div>
                     </div>
