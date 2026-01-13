9c25dcee4adb3110237c8dc86e00d4c5e0e5c83b
silvadealmeida <63240094+silvadealmeida@users.noreply.github.com>
2026-01-13 10:41:21 +0000

    [create-pull-request] automated change

--- geonode_mapstore_client/static/mapstore/dist/js/65522.9527fa89de7f43c6.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkgeonode_mapstore_client=self.webpackChunkgeonode_mapstore_client||[]).push([[65522],{865522:(e,r,n)=>{n.r(r),n.d(r,{default:()=>a});var s=n(927885),t=n(203704);class a extends t.Z{decodeBlock(e){return(0,s.rr)(new Uint8Array(e)).buffer}}}}]);
